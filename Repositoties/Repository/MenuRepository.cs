09cfcfc2fd4b5706d7d3b3e64204c2a26712c1d2
cyrus79-unlimit <giahuy12377@gmail.com>
2024-02-25 18:15:21 +0700

    Menu Controller and OData lib

---
+++ Repositoties/Repository/MenuRepository.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObject.Models;
+using DataAccess;
+using Repositoties.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositoties.Repository
+{
+    public class MenuRepository : IMenuRepository
+    {
+        public void DeleteMenu(Menu menu) => MenuDao.DeleteMenu(menu);
+
+        public List<Menu> GetAllMenus() => MenuDao.GetAllMenus();
+
+        public Menu GetMenuById(int menuId) => MenuDao.GetMenuByID(menuId);
+
+        public void SaveMenu(Menu menu) => MenuDao.SaveMenu(menu);
+
+        public void UpdateMenu(Menu room) => MenuDao.UpdateMenu(room);
+    }
+}
